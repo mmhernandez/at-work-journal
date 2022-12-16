fa0075216fbaa54755e2a786d5cd6981337093fe
mmhernandez <mmdangelo11@gmail.com>
2022-12-16 16:24:38 -0700

    updating js file

--- journal/script.js
+++ journal/script.js
@@ -1,7 +1,7 @@
 var entryLogElement = document.querySelector("#entry-log");
 
 function addEntry(event) {
-    //prevent default form actin to append contents of form to URL
+    //prevent default form action to append contents of form to URL
     event.preventDefault();
     
     var newEntryElement = document.querySelector("#entry");
@@ -9,6 +9,14 @@ function addEntry(event) {
 
     newEntryElement.value = '';
 }
+
+// ALTERNATE SYNTAX OPTION - when using ID's, don't need to create variables using querySelector or getElementByID, b/c javascript knows the IDs from the html
+// WOULD NEED TO UPDATE THE FUNCTION CALL FROM THE HTML
+// function addEntry(event, entry, entryLog) {
+//     event.preventDefault();
+//     entryLog.innerHTML += `<p>${entry.value}</p>`;
+//     entry.value = '';
+// }
 
 function hide(element) {
     if(element.innerText == "My Journal") {
